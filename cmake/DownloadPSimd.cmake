db0a8859fa460d27acea9b9955af75219b8fd6f8
TheMarpe <martin.peterlin7@gmail.com>
2021-07-12 20:29:00 +0200

    Added additional prints

--- cmake/DownloadPSimd.cmake
+++ cmake/DownloadPSimd.cmake
@@ -2,14 +2,19 @@ CMAKE_MINIMUM_REQUIRED(VERSION 2.8.12 FATAL_ERROR)
 
 PROJECT(psimd-download NONE)
 
+execute_process(COMMAND which git)
+execute_process(COMMAND git --version)
+
 INCLUDE(ExternalProject)
 ExternalProject_Add(psimd
 	GIT_REPOSITORY https://github.com/Maratyszcza/psimd.git
 	GIT_TAG origin/master
 	SOURCE_DIR "${CMAKE_BINARY_DIR}/psimd-source"
 	BINARY_DIR "${CMAKE_BINARY_DIR}/psimd"
-	CONFIGURE_COMMAND ""
-	BUILD_COMMAND ""
+	CONFIGURE_COMMAND "which" "git"
+	BUILD_COMMAND "bash" "-c" "$(which git) --version"
 	INSTALL_COMMAND ""
 	TEST_COMMAND ""
 )
+
+execute_process(COMMAND cat "${CMAKE_BINARY_DIR}/psimd-download/psimd-prefix/tmp/psimd-gitclone.cmake")
